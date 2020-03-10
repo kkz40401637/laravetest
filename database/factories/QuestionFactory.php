7dee96c8480226534ee089f743d74218c23bebb0
kkz40401637 <kaungkhantzin123@gmail.com>
2020-03-10 13:37:05 +0630

    factory and seed

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -5,8 +5,22 @@
 use App\Model\Question;
 use Faker\Generator as Faker;
 
+use App\Model\Category;
+use App\User;
+
 $factory->define(Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title' => $title,
+        'slug' => $title,
+        'body' => $faker->text,
+        'category_id' => function() {
+            return Category::all()->random();
+        },
+
+        'user_id' => function() {
+            return User::all()->random();
+        }
+
     ];
 });
